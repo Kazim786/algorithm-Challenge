e080d5146b1069add64a667ae7e32821112201e3
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-02-18 10:03:31 -0600

    list of characters complete

--- GOThrones.js
+++ GOThrones.js
@@ -3,5 +3,17 @@ fetch("https://www.anapioficeandfire.com/api/characters?page=1&pageSize=50")
     return response.json();
 })
 .then((characters)=>{
-    console.log(characters)
+    var i;
+for (i = 0; i < characters.length; i++) {
+    console.log(characters[i]);
+    let body = document.getElementsByTagName("ul")
+    var names = document.createElement("li");
+    names.innerHTML = characters[i].name
+    body[0].appendChild(names);
+
+
+}
 })
+
+
+
